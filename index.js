0775963950c67193b82477f03492866e9e9cb05d
Piyush <piyushsingla8803@gmail.com>
2024-06-22 14:16:33 +0530

    seventh

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const __dirname = dirname(fileURLToPath(import.meta.url));
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -7,17 +8,17 @@ import dotenv from "dotenv";
 import multer from "multer";
 import { readFile } from 'node:fs';
 import fs from "fs";
-import { upload_file } from "./cloudinary.js";
+import { upload_file } from __dirname+"/cloudinary.js";
 // import csv from "csv-parser";
 import util from "util"
 import csv from "csvtojson";
 import { json2csv } from "csvjson-json2csv";
-import {topsis} from "./function.js"
-import { email_send } from "./email.js";
-import {deleteFirstColumn} from "./delete_first_column.js";
+import {topsis} from  __dirname+"/function.js"
+import { email_send } from  __dirname+"/email.js";
+import {deleteFirstColumn} from  __dirname+"/delete_first_column.js";
 dotenv.config()
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+
 
 const app=express();
 const port=process.env.PORT;
